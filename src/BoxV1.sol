6a1cfc30987c6dcd1c7e6b51109ca6b934d38f1c
infoparity <infoparity@protonmail.com>
2023-08-23 10:21:32 -0500

    added basic tests and contracts for UUPS upgrade pattern

---
+++ src/BoxV1.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifer: MIT
+pragma solidity ^0.8.18;
+
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+// storage is stored in the proxy, NOT the implementation
+
+// Proxy -> deploy implementation -> call some "initializer" function.
+
+contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    uint256 internal number;
+
+    // @custom: oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers(); // don't let any initialization happen
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(); // sets owner to msg.sender
+        __UUPSUpgradeable_init();
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
